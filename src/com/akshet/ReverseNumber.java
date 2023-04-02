49e5d81fc75f3041d76f24907cf98304f62ba4c2
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-02 17:02:37 +0530

    Using Two approach Basic and Recursive one in ReverseNumber.java file

---
+++ src/com/akshet/ReverseNumber.java
@@ -0,0 +1,42 @@
+package com.akshet;
+
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void ReversedNumber(int num){
+        int temp=num;
+        int rNumber=0;
+        int rem;
+        while(temp>0){
+            rem=temp%10;
+            rNumber= (rNumber*10) + rem;
+            temp=temp/10;
+        }
+        System.out.println("Using Basic Approach! ");
+        System.out.println("The Reverse of the number "+num+" is: "+rNumber);
+    }
+
+    public static int ReversedNumberRecursion(int num, int rNumber){
+        if(num<=0)
+            return rNumber;
+
+        int temp=num;
+        int rem= temp%10;
+        rNumber= rNumber*10 + rem;
+        temp=temp/10;
+
+        return ReversedNumberRecursion(temp,rNumber);
+    }
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number you want to reverse");
+        int num=scan.nextInt(); // 123456789
+        System.out.println("Entered number is: "+ num);
+
+        ReversedNumber(num);
+
+        System.out.println("Using Recursive Approach! ");
+        System.out.println("The Reverse of the number "+num+" is: "+ReversedNumberRecursion(num,0));
+    }
+}
