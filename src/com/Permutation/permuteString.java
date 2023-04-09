7e4760869a57758c2014ddccfbbc4760e2eed5f1
AKSHET PATIAL <akshet.patial@gmail.com>
2023-04-09 15:52:43 +0530

    Created the class permuteString.java

--- src/com/Permutation/permuteString.java
+++ src/com/Permutation/permuteString.java
@@ -1,13 +1,37 @@
 package com.Permutation;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class permuteString {
 
-    public static String[] perSeq(String st){
+    public static ArrayList<String> perSeq(String st) {
+        return helperFunction(st, "");
+    }
 
-        return new String[]{""};
+    private static ArrayList<String> helperFunction(String given, String ans) {
+        ArrayList<String> finalString = new ArrayList<>();
+
+        if (given.length()==0) {
+            finalString.add(ans);
+           //System.out.println(ans);
+            return finalString;
+        }
+
+        // Taking the character
+        char ch = given.charAt(0);
+
+        // As the recursive call will be equals to size of the ans String +1 ;
+        for(int i=0;i<=ans.length();i++){
+            // checking the places where we can add the character
+            String first = ans.substring(0,i);
+            String others = ans.substring(i,ans.length());
+            // Only put ans = first+ch+others in the recursive if put in the loop body then when recursive call goes back it
+            // Will remain with the  value and will not hold the previous value.
+            ArrayList<String> prevString =   helperFunction(given.substring(1), first+ch+others);
+            finalString.addAll(prevString);
+        }
+        return finalString;
     }
 
     public static void main(String[] args) {
@@ -15,8 +39,8 @@ public class permuteString {
         Scanner scan = new Scanner(System.in);
         String st = scan.nextLine();
 
-        System.out.println("Adjacent Sequences of the String "+st+" is : ");
-        String[] permutations = perSeq(st);
-        System.out.println(Arrays.toString(permutations));
+        System.out.println("Permutation Sequences of the String " + st + " is : ");
+        ArrayList<String> permutations = perSeq(st);
+        System.out.println(permutations);
     }
 }
